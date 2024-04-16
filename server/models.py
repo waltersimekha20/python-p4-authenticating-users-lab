4f1cc48bd3c85f949ec9d04741fbc85b04e9293d
waltersimekha <waltersimekha@gmail.com>
2024-04-16 15:37:28 +0300

    done

--- server/models.py
+++ server/models.py
@@ -1,12 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
-
-db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy()
 
 class Article(db.Model, SerializerMixin):
     __tablename__ = 'articles'
@@ -17,6 +12,7 @@ class Article(db.Model, SerializerMixin):
     content = db.Column(db.String)
     preview = db.Column(db.String)
     minutes_to_read = db.Column(db.Integer)
+    is_member_only = db.Column(db.Boolean, default=False)
     date = db.Column(db.DateTime, server_default=db.func.now())
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
